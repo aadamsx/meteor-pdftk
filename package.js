39681649570d31a7a6d78856207b45b1784aee3b
aadamsx <aadams99@gmail.com>
2014-12-25 21:47:26 -0600

    added npm reference

--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
   name: 'aadams:pdftk',
   summary: 'PDFTK wrapper for Meteor',
-  version: '0.1.2',
+  version: '0.1.3',
   git: 'https://github.com/aadamsx/meteor-pdftk.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
-  api.addFiles('pdftkWrapper.js');
+  api.use(['underscore'], 'server');
+  api.export('PDFTK');
+  api.addFiles('pdftkWrapper.js', 'server');
 });
 
 Package.onTest(function(api) {
