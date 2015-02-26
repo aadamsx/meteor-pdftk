6c7a08924219e24a069e96b450ba6fb97cc80dc0
aadamsx <aadams99@gmail.com>
2015-02-26 17:29:37 -0600

    update
    underscore should already be available

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'aadams:pdftk',
   summary: 'Wrapper for PDFTK: PDF form fill, concatination, watermark, stamp and more',
-  version: '0.1.12',
+  version: '0.1.14',
   git: 'https://github.com/aadamsx/meteor-pdftk.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom(['METEOR@0.9.3', 'METEOR@1.0']);
-  api.use(['underscore'], 'server');
+  // api.use(['underscore'], 'server');
   api.export('PDFTK');
   api.addFiles('pdftk-wrapper.js', 'server');
 });
