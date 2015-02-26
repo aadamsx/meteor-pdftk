b037047c5b6212cd4bddfe8c3e5d610942377621
aadamsx <aadams99@gmail.com>
2015-02-26 17:26:03 -0600

    update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'aadams:pdftk',
-  summary: 'Wrapper for PDFTK: PDF form fill, real concatination, file merge, split, encrypt, decrypt, rotate, watermark, stamp and more',
+  summary: 'Wrapper for PDFTK: PDF form fill, concatination, watermark, stamp and more',
   version: '0.1.12',
   git: 'https://github.com/aadamsx/meteor-pdftk.git',
   documentation: 'README.md'
--- pdftk-wrapper.js
+++ pdftk-wrapper.js
@@ -5,9 +5,9 @@ if (PDFTK === undefined)
   PDFTK = {};
 
 /**
- * Execute PTFTK in command line
- * @param  {array}    args: pdftk args
- * @param  {Function} callback: ...hum... the callback!
+ * Execute the PDFTK installed in the system
+ * @param  {Array}    args - command-line arguments to pdftk
+ * @param  {Function} callback - callback function that receives error, stdout, and stderr
  */
 PDFTK.execute = function (args, callback) {
   var command = 'pdftk ' + args.join(' ');
@@ -18,20 +18,41 @@ PDFTK.execute = function (args, callback) {
   });
 };
 
-/**
- * Methodes return a Buffer with result PDFTK command
- */
 
-PDFTK.pages = function(pdf, start, end, callback) {
+/**
+ * Assemble page range from the input PDF to create a new PDF. Read more at https://www.pdflabs.com/docs/pdftk-man-page/#dest-op-cat
+ * @param {String}      pdf - Path to input PDF file
+ * @param {String}      start
+ * @param {String}      end
+ * @param {String}      output
+ * @param {Function}    callback
+ * @return {Npm.buffer} Node.js Buffer with the result of executing the pdftk command
+ */
+PDFTK.pages = function(pdf, start, end, output, callback) {
   var range = (start || 1) + '-' + (end || 'end');
-  PDFTK.execute([pdf, 'cat', range, 'output -'], callback);
+  PDFTK.execute([pdf, 'cat', range, 'output ', output], callback);
 };
 
+/**
+ * Fills the single input PDF's form fields with the data from an FDF or XFDF file. Read more at https://www.pdflabs.com/docs/pdftk-man-page/#dest-op-fill-form
+ * @param {String}      pdf - path to the input PDF file
+ * @param {String}      xfdf - path to the (X)FDF file
+ * @param {String}      output - path to the output PDF file
+ * @param {Function}    callback
+ * @return {Npm.buffer} Node.js Buffer with the result of executing the pdftk command
+ */
 PDFTK.fillForm = function(pdf, xfdf, output, callback) {
   PDFTK.execute([pdf, 'fill_form ', xfdf, 'output ', output], callback);
 };
 
 // todo: add page ranges and file handles
+/**
+ * Assemble pages from the input PDF(s) to create a new PDF. Read more at https://www.pdflabs.com/docs/pdftk-man-page/#dest-op-cat
+ * @param {first: pdf1, second: pdf2, third: pdf3, forth: pdf4, fifth: pdf5}     pdf# - Path to input PDF file
+ * @param {String}      output
+ * @param {Function}    callback
+ * @return {Npm.buffer} Node.js Buffer with the result of executing the pdftk command
+ */
 PDFTK.cat = function(inputs, output, callback) {
   var params = _.defaults(inputs,
     {first: 'undefined', second: 'undefined', third: 'undefined', forth: 'undefined', fifth: 'undefined'}
@@ -48,9 +69,25 @@ PDFTK.cat = function(inputs, output, callback) {
 };
 
 /**
- * Stamp
- * Ex: pdftk sample.pdf stamp stamp.pdf output -
+ * Stamp the input PDF with the `stamp` file and produce an `output` PDF. Read more at https://www.pdflabs.com/docs/pdftk-man-page/#dest-op-stamp
+ * @param {String}      pdf - path to the input PDF file
+ * @param {String}      stamp - path to the stamp PDF file
+ * @param {String}      output - path to the output PDF file
+ * @param {Function}    callback
+ * @return {Npm.buffer} Node.js Buffer with the result of executing the pdftk command
  */
 PDFTK.stamp = function(pdf, stamp, output, callback) {
   PDFTK.execute([pdf, 'stamp', stamp, 'output', output], callback);
+};
+
+/**
+ * Stamp the input PDF with each page of the `stamp` file and produce an `output` PDF. Read more at https://www.pdflabs.com/docs/pdftk-man-page/#dest-op-multistamp
+ * @param {String}      pdf - path to the input PDF file
+ * @param {String}      stamp - path to the stamp PDF file
+ * @param {String}      output - path to the output PDF file
+ * @param {Function}    callback
+ * @return {Npm.buffer} Node.js Buffer with the result of executing the pdftk command
+ */
+PDFTK.multistamp = function multistamp(pdf, stamp, output, callback) {
+  return PDFTK.execute([pdf, 'multistamp', stamp, 'output', output], callback);
 };
